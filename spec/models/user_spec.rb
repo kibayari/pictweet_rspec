34b9fc73fc25c434acafb11765300f2bd9a06906
kibayari <kibayari09@gmail.com>
2020-08-17 11:18:46 +0900

    includeへの書き換え

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ describe User, type: :model do
     it "nicknameが空だと登録できない" do
       user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
       user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      binding.pry
     end
     it "emailが空では登録できない" do
     end
