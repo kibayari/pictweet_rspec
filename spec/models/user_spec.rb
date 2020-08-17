c6691746fb7b79db379ef294718ac82f03357aea
kibayari <kibayari09@gmail.com>
2020-08-17 14:17:35 +0900

    exampleテストコード編集

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,16 @@ describe User, type: :model do
    describe 'ユーザー新規登録' do
     context '新規登録がうまくいくとき' do
       it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
+        expect(@user).to be_valid
       end
       it "nicknameが6文字以下で登録できる" do
+        @user.nickname = "aaaaaa"
+        expect(@user).to be_valid
       end
       it "passwordが6文字以上であれば登録できる" do
+        @user.password = "000000"
+        @user.password_confirmation = "000000"
+        expect(@user).to be_valid
       end
     end
 
