39cbee37a7fab00e9049fa302aa3d6972e08f7a7
kibayari <kibayari09@gmail.com>
2020-08-18 14:16:38 +0900

    新規登録がうまく行かないとき

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -28,16 +28,29 @@ RSpec.describe 'ユーザー新規登録', type: :system do
         find(".user_nav").find("span").hover
       ).to have_content('ログアウト')
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ユーザー新規登録ができないとき' do
     it '誤った情報ではユーザー新規登録ができずに新規登録ページへ戻ってくる' do
       # トップページに移動する
+      visit root_path
       # トップページにサインアップページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: ""
+      fill_in 'Email', with: ""
+      fill_in 'Password', with: ""
+      fill_in 'Password confirmation', with: ""
       # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(0)
       # 新規登録ページへ戻されることを確認する
+      expect(current_path).to eq "/users"
     end
   end 
 end
