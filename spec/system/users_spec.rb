b13d3af7a28d8cd9e216ea0346ca17e5b4f4ccd8
kibayari <kibayari09@gmail.com>
2020-08-18 12:27:33 +0900

    topへ遷移する

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -11,9 +11,18 @@ RSpec.describe 'ユーザー新規登録', type: :system do
       # トップページにサインアップページへ遷移するボタンがあることを確認する
       expect(page).to have_content('新規登録')
       # 新規登録ページへ移動する
+      visit new_user_registration_path
       # ユーザー情報を入力する
+      fill_in 'Nickname', with: @user.nickname
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
+      fill_in 'Password confirmation', with: @user.password_confirmation
       # サインアップボタンを押すとユーザーモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
       # トップページへ遷移する
+      expect(current_path).to eq root_path
       # カーソルを合わせるとログアウトボタンが表示されることを確認する
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
     end
